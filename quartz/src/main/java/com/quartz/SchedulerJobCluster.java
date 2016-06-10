316cc243374611d3899052513773c276deafdba1
pzxwhc <wuhuachuan712@163.com>
2016-06-11 02:19:21 +0800

    ADD quarz cluster

--- quartz/src/main/java/com/quartz/SchedulerJobCluster.java
+++ quartz/src/main/java/com/quartz/SchedulerJobCluster.java
@@ -4,24 +4,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.CronTrigger;
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 /**
- * Created by wuhuachuan on 16/6/9.
+ * Created by wuhuachuan on 16/6/11.
  *
- * 该类主要用于单机版本
+ * 该类主要用于集群版本
  */
 
 @Slf4j
-//@Configuration
-public class SchedulerJob {
+@Configuration
+public class SchedulerJobCluster {
 
-    private final static String CRON_EXPRESSION = "*/3 * * * * ?";
+    private final static String CRON_EXPRESSION = "*/5 * * * * ?";
 
     @Bean
     public JobDetailFactoryBean createJobDetail(){
@@ -47,12 +53,25 @@ public class SchedulerJob {
     }
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(List<Trigger> triggers){
+    public SchedulerFactoryBean schedulerFactoryBean(List<Trigger> triggers, DataSource dataSource){
         SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
-        schedulerFactoryBean.setSchedulerName("myScheduleName");
 
         schedulerFactoryBean.setTriggers(triggers.toArray(new Trigger[0]));
+        schedulerFactoryBean.setQuartzProperties(quartzProperties());
+        schedulerFactoryBean.setDataSource(dataSource);
 
         return schedulerFactoryBean;
+    }
+
+    private Properties quartzProperties(){
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
+        try {
+            propertiesFactoryBean.afterPropertiesSet();
+            return propertiesFactoryBean.getObject();
+        } catch (IOException e) {
+            log.error("read quartz.properties file error: {}", e.getMessage());
+        }
+        return null;
     }
 }
